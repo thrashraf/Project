c933f8aa8b3aabc2fcac9ff2a70862c7bb305765
Isokayy <fareezdaniel34@gmail.com>
2022-05-08 17:10:35 +0800

    update categories

--- frontend/src/components/Categories.tsx
+++ frontend/src/components/Categories.tsx
@@ -15,7 +15,7 @@ return (
       className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center w-30 "
       onClick={() => props.setIsOpen(!props.isOpen)}
     >
-      YEAR {""}
+      Filter By {""}
       <i className="ml-2 fa-solid fa-angle-down" />
     </button>
 
